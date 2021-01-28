d5aba62df6115de6e3568ee00b3989a31ed13000
Steven Tong <steven.c.tong@gmail.com>
2021-01-28 08:56:29 -0800

    Updated Get-Delete-Snapshot

--- powershell/Get-Delete-SnapshotManagement.ps1
+++ powershell/Get-Delete-SnapshotManagement.ps1
@@ -19,6 +19,11 @@ You can have the script delete the snapshots by setting $delete to $true.
 Or you can edit the CSV to contain the snapshots you want deleted and have the script read the CSV file and delete those snapshots.
 When deleting snapshots the script will first attempt to set the snpashots to SLA "UNPROTECTED" and then delete the snapshots
 
+Possible Use Cases:
+- Delete all Relic snapshots older than a certain date
+- Get a list of all On Demand Snapshots and see if they are assigned to the right SLA
+- Get a list of all Retrieved snapshots that are using up cluster capacity
+
 .NOTES
 Written by Steven Tong for community usage
 GitHub: stevenctong
@@ -88,18 +93,22 @@ Import-Module Rubrik
 $apiVer = 'internal'
 
 # Get snapshots before this date. Set to '' if you want all snapshots without filtering by date.
-# $beforeDate = (Get-Date -Year 2020 -Month 09 -Day 01)
+#$beforeDate = (Get-Date -Year 2020 -Month 6 -Day 1)
 $beforeDate = ''    # Set to '' if you want all snapshots
 
 # Object Type to filter by:
 # VirtualMachine, MssqlDatabase, LinuxFileset, WindowsFileset, ShareFileset, NutanixVirtualMachine,
 # HypervVirtualMachine, ManagedVolume, Ec2Instance, StorageArrayVolumeGroup, VcdVapp, LinuxHost,
 # WindowsHost, OracleDatabase, VolumeGroup, AppBlueprint
-$objectType = 'LinuxFileset'
+$objectType = ''
 
 # Object Status to filter by:
 # Protected, Relic, Unprotected, ReplicatedRelic, RemoteUnprotected
 $objectStatus = ''
+
+# Snapshot Type to filter by:
+# OnDemand, Relic, Retrieved, Unprotected
+$snapshotType = ''
 
 # CSV file info
 $date = Get-Date
@@ -110,6 +119,7 @@ $csvFile = "./rubrik_snapshot_list-$($date.ToString("yyyy-MM-dd_HHmm")).csv"
 ###### RUBRIK AUTHENTICATION - BEGIN ######
 # First try using API token, then username/password if a user is provided, then credential file
 try {
+  if (!$server) { $server = Read-Host "Rubrik hostname or IP" }
   if ($token) { Connect-Rubrik -Server $server -Token $token }
   else {
     if ($user) {
@@ -142,31 +152,39 @@ if ($csvDelete -eq '')
   foreach ($i in $allSnapshotManagement) {
     # Process objects that match the object type or status filter
     if (($objectType -eq '' -or $i.objectType -like $objectType) -and
-        ($objectStatus -eq '' -or $i.unmanagedStatus -like $objectStatus)) {
+        ($objectStatus -eq '' -or $i.unmanagedStatus -like $objectStatus))
+    {
 
       # Get all snapshots for the object
       $objectSnapshots = Invoke-RubrikRESTCall -Method GET -Api $apiVer -Endpoint "unmanaged_object/$($i.id)/snapshot"
 
       # Iterate through each all the snapshots and filter for snapshots older than $beforeDate
       foreach ($j in $objectSnapshots.data) {
-        if ($beforedate -eq '' -or [DateTime]($j.date) -le $beforeDate)
+
+        if ($apiVer -like 'internal') {
+          $curSnapshotType = $j.unmanagedSnapshotType
+        } else {
+          $curSnapshotType = $j.snapshotType
+        }
+
+        if (($beforedate -eq '' -or [DateTime]($j.date) -le $beforeDate) -and
+            ($snapshotType -eq '' -or $curSnapshotType -like $snapshotType))
         {
-          $newObj = New-Object PSCustomObject
-
-          # Object properties
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectName" -Value $i.name
+          # Object + snapshot properites
           $objLoc = $i.physicalLocation.name -join ","
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectLocation" -Value $objLoc
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectType" -Value $i.objectType
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectUnmanagedStatus" -Value $i.unmanagedStatus
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectRetentionSlaDomainName" -Value $i.retentionSlaDomainName
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectTotalLocalStorage" -Value $i.localStorage
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectTotalArchiveStorage" -Value $i.archiveStorage
-          $newObj | Add-Member -MemberType NoteProperty -Name "objectID" -Value $i.id
 
-          # Snapshot properties
-          $newObj | Add-Member -MemberType NoteProperty -Name "snapshotID" -Value $j.id
-          $newObj | Add-Member -MemberType NoteProperty -Name "snapshotDate" -Value $j.date
+          $newObj = [PSCustomObject]@{
+            objectName = $i.name
+            objectLocation = $objLoc
+            objectType = $i.objectType
+            objectUnmanagedStatus =$i.unmanagedStatus
+            objectRetentionSlaDomainName = $i.retentionSlaDomainName
+            objectTotalLocalStorage = $i.localStorage
+            objectTotalArchiveStorage = $i.archiveStorage
+            objectID = $i.id
+            snapshotID = $j.id
+            snapshotDate = $j.date
+          }
 
           # If using API version 'internal', get these additional properties
           if ($apiVer -like 'internal')
@@ -226,15 +244,32 @@ if ($delete -eq $true)
 
       Write-Host "Deleting object: $currentObject, snapshots $deleteArray" -ForegroundColor Green
 
-      # First assign the snapshot to "UNPROTECTED" in order to delete it
+      # First assign the snapshot to "UNPROTECTED" in order to delete it, try v2 endpoint
       try {
         $assignBody = [PSCustomObject] @{
+          objectId = $currentObject
           slaDomainId = "UNPROTECTED"
           snapshotIds = $deleteArray
         }
-        Invoke-RubrikRESTCall -Method POST -Api $apiVer -Endpoint "unmanaged_object/snapshot/assign_sla" -Body $assignBody
+        $req = Invoke-RubrikRESTCall -Method POST -Api 2 -Endpoint "sla_domain/assign_to_snapshot" -Body $assignBody
       }
-      catch { Write-Error "Error assigning SLA UNPROTECTED to object $currentObject, snapshots $deleteArray" }
+      catch {
+        # If v2 endpoint not there or fails, use $apiVer endpoint
+        try {
+          $assignBody = [PSCustomObject] @{
+            slaDomainId = "UNPROTECTED"
+            snapshotIds = $deleteArray
+          }
+          $req = Invoke-RubrikRESTCall -Method POST -Api $apiVer -Endpoint "unmanaged_object/snapshot/assign_sla" -Body $assignBody
+        }
+        catch
+        {
+          Write-Error "Error assigning SLA UNPROTECTED to object $currentObject, snapshots $deleteArray"
+        }
+      }
+
+      # Snapshot SLA assignment is async, wait a little before attempting deletion
+      Start-Sleep -s 1
 
       # Delete the snapshot
       try
@@ -259,15 +294,32 @@ if ($delete -eq $true)
   # Delete the last processed object and associated snapshots
   Write-Host "Deleting object: $currentObject, snapshots $deleteArray" -ForegroundColor Green
 
-  # First assign the snapshot to "UNPROTECTED" in order to delete it
+  # First assign the snapshot to "UNPROTECTED" in order to delete it, try v2 endpoint
   try {
     $assignBody = [PSCustomObject] @{
+      objectId = $currentObject
       slaDomainId = "UNPROTECTED"
       snapshotIds = $deleteArray
     }
-    Invoke-RubrikRESTCall -Method POST -Api $apiVer -Endpoint "unmanaged_object/snapshot/assign_sla" -Body $assignBody
+    $req = Invoke-RubrikRESTCall -Method POST -Api 2 -Endpoint "sla_domain/assign_to_snapshot" -Body $assignBody
   }
-  catch { Write-Error "Error assigning SLA UNPROTECTED to object $currentObject, snapshots $deleteArray" }
+  catch {
+    # If v2 endpoint not there or fails, use $apiVer endpoint
+    try {
+      $assignBody = [PSCustomObject] @{
+        slaDomainId = "UNPROTECTED"
+        snapshotIds = $deleteArray
+      }
+      $req = Invoke-RubrikRESTCall -Method POST -Api $apiVer -Endpoint "unmanaged_object/snapshot/assign_sla" -Body $assignBody
+    }
+    catch
+    {
+      Write-Error "Error assigning SLA UNPROTECTED to object $currentObject, snapshots $deleteArray"
+    }
+  }
+
+  # Snapshot SLA assignment is async, wait a little before attempting deletion
+  Start-Sleep -s 1
 
   # Delete the snapshot
   try
@@ -278,6 +330,12 @@ if ($delete -eq $true)
     Invoke-RubrikRESTCall -Method POST -Api 1 -Endpoint "data_source/$($currentObject)/snapshot/bulk_delete" -Body $deleteBody
   } catch { Write-Error "Error deleting object $currentObject, snapshots $deleteArray" }
 
+  Write-Host "Finished attempting to delete snapshots. You may see warnings which are ok."
+  Write-Host "Snapshot SLA assignment is async which may have stalled some deletions from occurring."
+  Write-Host "Run script again if needed and generate another list of the snapshots to verify if still exist on the cluster or not."
+
 }  # if $delete is $true
+
+
 
 $disconnect = Disconnect-Rubrik -Confirm:$false
