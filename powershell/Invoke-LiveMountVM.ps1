48f535ff767c0f5b824e75d303fd5d6bb831928a
Steven Tong <steven.c.tong@gmail.com>
2021-09-24 21:25:10 -0700

    Rename Invoke-LiveMountVM.ps1

---
+++ powershell/Invoke-LiveMountVM.ps1
@@ -0,0 +1,83 @@
+#requires -modules Rubrik
+
+# https://build.rubrik.com
+# https://www.rubrik.com/blog/get-started-rubrik-powershell-module/
+# https://github.com/rubrikinc/rubrik-sdk-for-powershell
+# https://github.com/rubrikinc/rubrik-scripts-for-powershell
+
+# Written by Steven Tong for community usage
+# GitHub: stevenctong
+# Date: 9/22/21
+
+param (
+  [CmdletBinding()]
+
+  # Rubrik cluster hostname or IP address
+  [Parameter(Mandatory=$false)]
+  [string]$server = '',
+
+  # Use API token for authentication
+  [Parameter(Mandatory=$false)]
+  [string]$token = $null,
+
+  # Use Rubrik username/password for authentication
+  [Parameter(Mandatory=$false)]
+  [string]$user = $null,
+
+  # Use Rubrik username/password for authentication
+  [Parameter(Mandatory=$false)]
+  [string]$password = $null,
+
+  # Use credential file for authentication, location of it
+  [Parameter(Mandatory=$false)]
+  [string]$rubrikCred = 'rubrik_cred.xml'
+)
+
+Import-Module Rubrik
+
+
+###### VARIABLES - BEGIN ######
+
+$vmName = 'am1-stevtong-l1'
+$liveMountCount = 2
+
+# Use "MM/DD/YYYY HH:MM" format
+$liveMountDate = '09/22/2021 12:00'
+
+###### VARIABLES - END #######
+
+
+###### RUBRIK AUTHENTICATION - BEGIN ######
+# First try using API token, then username/password if a user is provided, then credential file
+try {
+  if ($token) { Connect-Rubrik -Server $server -Token $token }
+  else {
+    if ($user) {
+      if ($password) {
+        [SecureString]$password = ConvertTo-SecureString $password -AsPlainText -Force
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
+      }
+      else { $credential = Get-Credential -Username $user }
+    }
+    elseif (Test-Path $rubrikCred) { $credential  = Import-Clixml -Path $rubrikCred }
+    else { $credential = Get-Credential }
+    Connect-Rubrik -Server $server -Credential $credential
+  }
+} catch {
+  try {
+    Write-Error "Error connecting to cluster or with authentication."
+    $html = "Error connecting to cluster or with authentication."
+    if ($sendEmail) { Send-MailMessage -To $emailTo -From $emailFrom -Subject $emailSubject -BodyAsHtml -Body $html -SmtpServer $SMTPServer -Port $SMTPPort }
+    Exit 1
+  } catch { Exit 1 }
+}
+###### RUBRIK AUTHENTICATION - END ######
+
+$count = 0
+
+while ($count -lt $liveMountCount)
+{
+  Get-RubrikVM $vmName | Get-RubrikSnapshot -Date $liveMountDate | New-RubrikMount
+
+  $count += 1
+}
