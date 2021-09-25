ac881b141d50f90caa1502aade6a53d07c048003
Steven Tong <steven.c.tong@gmail.com>
2021-09-24 21:34:26 -0700

    Updated auth and added comments

--- powershell/Invoke-LiveMountVM.ps1
+++ powershell/Invoke-LiveMountVM.ps1
@@ -5,9 +5,35 @@
 # https://github.com/rubrikinc/rubrik-sdk-for-powershell
 # https://github.com/rubrikinc/rubrik-scripts-for-powershell
 
-# Written by Steven Tong for community usage
-# GitHub: stevenctong
-# Date: 9/22/21
+<#
+.SYNOPSIS
+Create multiple Live Mounts for a specified VM.
+
+.DESCRIPTION
+The Invoke-LiveMountVM script creates multiple Live Mounts for a specified VM.
+Specify the VM name and count with variables.
+
+.NOTES
+Written by Steven Tong for community usage
+GitHub: stevenctong
+Date: 9/22/21
+
+For authentication, use an API token (recommended), username/password, or a credential file.
+
+To create a credential file (note: only the user who creates it can use it):
+- Get-Credential | Export-CliXml -Path ./rubrik_cred.xml
+
+Fill out the PARAM section with config details for this script.
+
+.EXAMPLE
+./Invoke-LiveMountVM.ps1 -server <Rubrik_server> -token <API_token>
+Use an API token for authentication
+
+.EXAMPLE
+./Get-BackupSize.ps1 -server <Rubrik_server>
+Checks for credential file and if none found prompts for username/password.
+
+#>
 
 param (
   [CmdletBinding()]
@@ -38,11 +64,14 @@ Import-Module Rubrik
 
 ###### VARIABLES - BEGIN ######
 
-$vmName = 'am1-stevtong-l1'
-$liveMountCount = 2
+# Name of the VMware VM
+$vmName = 'vmname'
 
-# Use "MM/DD/YYYY HH:MM" format
-$liveMountDate = '09/22/2021 12:00'
+# The number of Live Mounts to spin up
+$liveMountCount = 1
+
+# The snapshot nearest this time to Live Mount, use "MM/DD/YYYY HH:MM" format
+$liveMountDate = '09/21/2021 12:00'
 
 ###### VARIABLES - END #######
 
@@ -72,6 +101,7 @@ try {
   } catch { Exit 1 }
 }
 ###### RUBRIK AUTHENTICATION - END ######
+
 
 $count = 0
 
