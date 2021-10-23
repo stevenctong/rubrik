58938adc1139f5a18e8ace974956a0144e4e8653
Steven Tong <steven.c.tong@gmail.com>
2021-10-22 23:20:07 -0700

    Updated to parse results with better outputs.

--- powershell/Get-VMwareDiskStats.ps1
+++ powershell/Get-VMwareDiskStats.ps1
@@ -3,10 +3,10 @@
 
 <#
 .SYNOPSIS
-Pulls VMware VM disk stats for Rubrik sizing.
+Pulls VMware VM and VMDK write throughput disk stats for Rubrik sizing.
 
 .DESCRIPTION
-The Get-VMwareDiskStats.ps1 script pulls VM disk stats for Rubrik sizing.
+The Get-VMwareDiskStats.ps1 script pulls VM and VMDK write throughput disk stats for Rubrik sizing.
 Requires PowerCLI and a vCenter read-only user.
 
 .NOTES
@@ -19,15 +19,20 @@ If there is not enough space in vCenter to save it for 5 days then run this scri
 Make sure that the data gathering covers the busiest days for the VMs.
 To configure stats collection, click on your vCenter -> Configure -> General -> Edit
 
-This script will attempt to gather 5 min stats for the last few days for a list of VMs.
-The results will be output to a separate CSV file for each day.
-Any pre-existing files will be overwritten.
+$critKBps and $warnKBps are set to highlight any VMDKs that we need to take a closer look at for sizing
+
+This script will attempt to gather 5 min stats for a list of VMs.
+Three different results files can be created:
+1) VM level stats - for each time period, the sum of all VM write throughput
+2) VMDK level stats - a list of any VMDK that has a write throughput greater than $warnKBps
+3) Raw stats that are gathered - if the # of rows are too large, the output will be split by day
 
 By default, the script will gather stats for all VMs listed in vCenter.
 You can also pass in a CSV with a list of VMs to gather stats for.
-A list of VMs in vCenter can be generated for editing.
+The list of VMs should have a column "Name" with a list of VMs to gather stats for.
+A list of VMs in vCenter can also be generated with this script which can be edited and then passed back in.
 
-If you are using Windows Powershell (not Core) you can also store your credentials using:
+If you are using Windows Powershell (not Core) you can store your credentials using:
 - New-VICredentialStoreItem
 
 .EXAMPLE
@@ -38,16 +43,13 @@ Prompts for vCenter info and gets stats for all VMs.
 Prompts for vCenter username & password and gets stats on all VMs.
 
 ./Get-VMwareDiskStats.ps1 -server <vcenter_server> -username <username> -password <password>
-Pass your vCenter credentials as parameters.
+Pass your vCenter credentials as parameters and get stats on all VMs.
 
 ./Get-VMwareDiskStats.ps1 -outputVMfile <vmlist.csv>
-Outputs a list of VMs from vCenter and exits script.
+Outputs a list of VMs from vCenter and exits script. Prompts for vCenter info.
 
 ./Get-VMwareDiskStats.ps1 -importVMfile <vmlist.csv>
-Imports a list of VMs to gather stats on. VMs should be under a "Name" column.
-
-./Get-VMwareDiskStats.ps1 -days #
-Specify how many days back to gather stats for, default 5 days.
+Imports a list of VMs to gather stats on. VMs should be under a "Name" column. Prompts for vCenter info.
 
 #>
 
@@ -72,20 +74,20 @@ param (
 
   # Specify a filename with a list of VMs to gather stats on using the "Name" column
   [Parameter(Mandatory=$false)]
-  [string]$importVMfile = $null,
-
-  # Number of days to gather stats for, default 5 days
-  [Parameter(Mandatory=$false)]
-  [int]$days = 5
+  [string]$importVMfile = $null
 )
 
 # Base filename to output the stats to
-$csvOutput = "./vmware_stats-"
+$vmCSVoutput = "./vm_level_disk_stats-"
+$vmdkCSVoutput = "./vmdk_level_disk_stats-"
+$rawCSVoutput = "./vm_raw_disk_stats-"
 
 # List of stats to gather
-$statList = @('virtualDisk.write.average', 'virtualDisk.read.average')
-# Max number samples to gather for 5 min interval
-$maxSamples5min = 300
+$statList = @('virtualDisk.write.average')
+# Max sustained KBps to highlight for a VMDK
+$critKBps = 50000
+# Warning sustained KBps to highlight for a VMDK
+$warnKBps = 40000
 
 ### Begin - PowerCLI authentication ###
 if (!$server) { $server = Read-Host "vCenter hostname or IP" }
@@ -96,6 +98,8 @@ if (!$user) {
   Connect-VIServer -server $server -user $user -password $password
 }
 ### End - PowerCLI authentication
+
+$date = Get-Date
 
 # Get a list of VMs, output to the specified CSV file, and exit script
 if ($outputVMfile -ne '')
@@ -109,30 +113,110 @@ if ($outputVMfile -ne '')
 if ($importVMfile -ne '')
 {
   Write-Host "`nImporting CSV file: $importVMfile`n" -foregroundcolor Green
-  $vmList = Import-CSV $importVMfile
+  $vmList = Import-CSV $importVMfile | Sort-Object
 } else {
-  $vmList = Get-VM
+  $vmList = Get-VM | Sort-Object
 }
 
-# Go $days back and gather stats for each day
-for ($i = $(1-$days); $i -lt 1; $i++)
+# Initialize arrays to hold the gathered data in
+$vmDataArray = @()
+$vmdkDataArray = @()
+
+# For each VM, gather status
+foreach ($vm in $vmList)
 {
-  $dataList = @()
-  $startDate = (Get-Date).AddDays($i).ToString("MM-dd-yyyy")
-  $finishDate = (Get-Date).AddDays($i+1).ToString("MM-dd-yyyy")
-
-  Write-Host "Getting stats for date: $startDate`n" -foregroundcolor green
-  foreach ($vm in $vmList)
+  Write-Host "Getting stats for VM: $($vm.Name)"
+  # Loop through each stat we want to gather
+  foreach ($stat in $statList)
   {
-    Write-Host "Getting stats for VM: $($vm.Name)"
-    foreach ($stat in $statList)
-    {
-      $data = Get-Stat -entity $vm.Name -stat $stat -maxSamples $maxSamples5min -interval 5 -start $startDate -finish $finishDate
+    $data = Get-Stat -entity $vm.Name -stat $stat -interval 5
 
-      $dataList += $data
+    # If there is no "Instance" (disk) value then add it to the VM level array
+    $vmDataArray += $data | Where Instance -eq ''
+
+    # If there is "Instance" (disk) value then add it to the VMDK level array
+    $vmdkDataArray += $data | Where Instance -ne ''
+  } # foreach ($stat in $statList)
+} # foreach ($vm in $vmList)
+
+# Initialize array to hold calculated results in
+$vmResults = @()
+
+# For VM level stats, group everything by Timestamp
+$vmGroup = $vmDataArray | Group-Object -Property Timestamp
+
+# For VM level stats, sum up the values for each Timestamp
+foreach ($i in $vmGroup)
+{
+  $KBpsPerTimestamp = [PSCustomObject] @{
+    KBps = ($i.group | Measure-Object -Property Value -Sum).Sum
+    Timestamp = $i.name
+    MetricId = $i.group[0].MetricId
+  }
+  $vmResults += $KBpsPerTimestamp
+} # foreach ($i in $vmGroup)
+
+# For each VMDK level stat, build a list of VMDKs whose Value are greater than $critKBps
+$critVMDKs = $vmdkDataArray | Where Value -ge $critKBps | Sort-Object -Property Value -Descending |
+  Select @{n='KBps'; e={$_.Value}}, @{n='VM'; e={$_.Entity}}, 'Instance', 'Timestamp', 'MetricId'
+
+# For each VMDK level stat, build a list of VMDKs whose Value are greater than $warnKBps
+$warnVMDKs = $vmdkDataArray | Where { $_.value -ge $warnKBps -and $_.value -lt $critKBps } |
+  Sort-Object -Property Value -Descending | Select @{n='KBps'; e={$_.Value}}, @{n='VM'; e={$_.Entity}}, 'Instance', 'Timestamp', 'MetricId'
+
+Write-Host ""
+Write-Host "These VMDKs had sustained write throughput that crossed the warning threshold of $warnKBPs KBps:" -foregroundcolor green
+$warnVMDKs | Format-Table
+
+Write-Host "These VMDKs had sustained write throughput that crossed the critical threshold of $critKBPs KBps:" -foregroundcolor green
+$critVMDKs | Format-Table
+
+Write-Host "Top 5 time time periods that had the highest sustained write throughput:" -foregroundcolor green
+$vmResults | Sort-Object KBps -Desc -Top 5 | Format-Table
+
+Write-Host
+Write-Host "Total times a VMDK crossed the warning threshold of $warnKBPs KBps: $($warnVMDKs.count)"
+Write-Host "Total times a VMDK crossed the critical threshold of $critKBPs KBps: $($critVMDKs.count)"
+Write-Host ""
+Write-Host "Number of unique VMs that crossed the warning threshold: $(($warnVMDKs.VM | Sort-Object | Unique).count)" -foregroundcolor green
+Write-Host "Number of unique VMs that crossed the critical threshold: $(($critVMDKs.VM | Sort-Object | Unique).count)" -foregroundcolor green
+Write-Host "Highest sustained write throughput: $(($vmResults.KBps | Measure -Maximum).maximum) KBps" -foregroundcolor green
+Write-Host
+
+$vmResults | Export-CSV -NoTypeInformation -Path $vmCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv
+Write-Host "VM level results output to: $vmCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv" -foregroundcolor green
+
+# Combine VMDKs that are above $warnVMDKs and $critVMDKs into one and output to a CSV file if there are results
+$critVMDKs += $warnVMDKs
+if ($critVMDKs.count -gt 0) {
+  $critVMDKs | Export-CSV -NoTypeInformation -Path $vmdkCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv
+  Write-Host "VMDK level results output to: $vmdkCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv" -foregroundcolor green
+} else {
+  Write-Host "No VMDK level results to output" -foregroundcolor green
+}
+
+Write-Host ""
+
+# Combine the raw results for the VM summary results and VMDK results
+$vmDataArray += $vmdkDataArray
+
+# Excel supports 1M rows. If the raw data is less than a large number, output raw results to a single file
+# Otherwise, split up outputting the raw data by date
+if ($vmDataArray.count -lt 800000)
+{
+  $vmDataArray | Select 'Timestamp', 'Value', 'Entity', 'Instance', 'MetricId', 'Unit', 'IntervalSecs' |
+    Export-CSV -NoTypeInformation -Path $rawCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv
+  Write-Host "Raw data captured output to: $rawCSVoutput$($date.ToString("yyyy-MM-dd_HHmm")).csv" -foregroundcolor green
+} else {
+  Write-Host "Splitting raw data output by date since number of rows was very large"
+  for ($days = 0; $days -gt -7; $days--) {
+    $dateComp = $date.date.adddays($days)
+
+    if (($vmDataArray | Where-object { $_.timestamp.date -eq $dateComp.date }).count -gt 0) {
+      $vmDataArray | Where-object { $_.timestamp.date -eq $dateComp.date } |
+        Select 'Timestamp', 'Value', 'Entity', 'Instance', 'MetricId', 'Unit', 'IntervalSecs' |
+        Export-CSV -NoTypeInformation -Path $rawCSVoutput$($dateComp.ToString("MM-dd"))_$($date.ToString("yyyy-MM-dd_HHmm")).csv
+      Write-Host "Raw data captured output to: $rawCSVoutput$($dateComp.ToString("MM-dd"))_$($date.ToString("yyyy-MM-dd_HHmm")).csv" -foregroundcolor green
     }
   }
-  # Exports the results as a CSV for each date
-  $dataList | Export-Csv -NoTypeInformation -Path ${csvOutput}${startDate}.csv
-  Write-Host "`nResults output to: ${csvOutput}${startDate}.csv`n" -foregroundcolor green
 }
