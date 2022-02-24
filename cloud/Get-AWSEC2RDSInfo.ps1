325470aef0c6411060845dc1260013d7b4518597
Steven Tong <steven.c.tong@gmail.com>
2022-02-24 13:45:21 -0800

    Updated script to gather RDS info

--- cloud/Get-AWSEC2RDSInfo.ps1
+++ cloud/Get-AWSEC2RDSInfo.ps1
@@ -4,12 +4,13 @@
 
 <#
 .SYNOPSIS
-Gets all EC2 instances with the # of attached volumes and total sizes of all volumes.
+Gets all EC2 and RDS instances with the # of attached volumes and provisioned sizes.
 
 .DESCRIPTION
-The 'Get-AWSEC2VolumeInfo.ps1' script gets all EC2 instances in the specified region(s).
+The 'Get-AWSEC2RDSInfo.ps1' script gets all EC2 instance and RDS info in the specified region(s).
 For each EC2 instance it grabs the total number of volumes and total size for all volumes.
-A summary of the total # of instances, # of volumes, and capacity will be output to console.
+For each RDS instance it grabs the total provisioned size.
+A summary of the total # of instances, # of volumes, RDS instance, and capacity will be output to console.
 
 A CSV file will be exported with the details.
 You should copy/paste the console output to send along with the CSV.
@@ -26,15 +27,16 @@ Date: 11/8/21
 Updated: 2/24/21
 
 .EXAMPLE
-./Get-EC2AWSVolumeInfo.ps1
+./Get-AWSEC2RDSInfo.ps1
 Runs the script to get all EC2 and associated volume info and output to a CSV file.
 
 #>
 
 $date = Get-Date
 
-# Filename of the CSV output
-$output = "aws_vol_info-$($date.ToString("yyyy-MM-dd_HHmm")).csv"
+# Filenames of the CSVs output
+$outputEc2Disk = "aws_vol_info-$($date.ToString("yyyy-MM-dd_HHmm")).csv"
+$outputRDS = "aws_rds_info-$($date.ToString("yyyy-MM-dd_HHmm")).csv"
 
 # List of AWS regions for reference, get current regions using 'Get-AWSRegion'
 $regionsUS = @( 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2')
@@ -58,10 +60,11 @@ Import-Module AWSPowerShell.NetCore
 Write-Host "Current identity:" -foregroundcolor green
 Get-STSCallerIdentity | format-table
 
-# Contains list of EC2 instances with volume capacity info
+# Contains list of EC2 instances and RDS with capacity info
 $ec2list = @()
+$rdsList = @()
 
-# For all specified regions get the EC2 instance and volume info
+# For all specified regions get the EC2 instance and RDS info
 foreach ($region in $regions) {
   Write-Host "Getting EC2 instance info for region: $region" -foregroundcolor green
   $ec2instances = (Get-EC2Instance -region $region).instances
@@ -89,17 +92,43 @@ foreach ($region in $regions) {
 
     $ec2list += $ec2obj
   }
+
+  Write-Host "Getting RDS info for region: $region" -foregroundcolor green
+  $rdsDBs = Get-RDSDBInstance -region $region
+
+  foreach ($rds in $rdsDBs) {
+    $rdsObj = [PSCustomObject] @{
+      "RDSInstance" = $rds.DBInstanceIdentifier
+      "SizeGiB" = $rds.AllocatedStorage
+      "SizeGB" = [math]::round($($rds.AllocatedStorage * 1.073741824), 3)
+      "RDSInstanceARN" = $rds.DBInstanceArn
+      "Region" = $region
+      "InstanceType" = $rds.DBInstanceClass
+      "Platform" = $rds.Engine
+    }
+
+    $rdsList += $rdsObj
+  }
 }
 
-$totalGiB = ($ec2list.sizeGiB | Measure -Sum).sum
-$totalGB = ($ec2list.sizeGB | Measure -Sum).sum
+$ec2TotalGiB = ($ec2list.sizeGiB | Measure -Sum).sum
+$ec2TotalGB = ($ec2list.sizeGB | Measure -Sum).sum
+
+$rdsTotalGiB = ($rdsList.sizeGiB | Measure -Sum).sum
+$rdsTotalGB = ($rdsList.sizeGB | Measure -Sum).sum
 
 Write-Host
 Write-Host "Total # of EC2 instances: $($ec2list.count)" -foregroundcolor green
 Write-Host "Total # of volumes: $(($ec2list.volumes | Measure -Sum).sum)" -foregroundcolor green
-Write-Host "Total capacity of all volumes: $totalGiB GiB or $totalGB GB" -foregroundcolor green
+Write-Host "Total capacity of all volumes: $ec2TotalGiB GiB or $ec2TotalGB GB" -foregroundcolor green
+
+Write-Host
+Write-Host "Total # of RDS instances: $(($rdsList.Database -ne '').count)" -foregroundcolor green
+Write-Host "Total provisioned capacity of all RDS instances: $rdsTotalGiB GiB or $rdsTotalGB GB" -foregroundcolor green
 
 # Export to CSV
 Write-Host ""
-Write-Host "CSV file output to: $output" -foregroundcolor green
-$ec2list | Export-CSV -path $output
+Write-Host "CSV file output to: $outputRDS" -foregroundcolor green
+$rdsList | Export-CSV -path $outputRDS
+Write-Host "CSV file output to: $outputEc2Disk" -foregroundcolor green
+$ec2List | Export-CSV -path $outputEc2Disk
