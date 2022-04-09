30c9c36c41c96845dadd76eae9cf5c19123ac181
Steven Tong <steven.c.tong@gmail.com>
2022-04-08 21:53:07 -0700

    Updated to support GraphQL endpoint

--- powershell/Get-Delete-SnapshotManagement.ps1
+++ powershell/Get-Delete-SnapshotManagement.ps1
@@ -28,6 +28,7 @@ Possible Use Cases:
 Written by Steven Tong for community usage
 GitHub: stevenctong
 Date: 12/20/20
+Modified: 4/8/22
 
 For authentication, use an API token (recommended), username/password, or a credential file.
 
@@ -90,7 +91,11 @@ Import-Module Rubrik
 
 # Depending on cluster version, set to either '1' or 'internal'
 # Use '1' for later cluster versions (v5.1+)
-$apiVer = 'internal'
+$apiVer = '1'
+
+# Depending on cluster version, set to either $true or $false
+# Whether to use the GraphQL endpoint to get the list of snapshot management
+$graphQL = $false
 
 # Get snapshots before this date. Set to '' if you want all snapshots without filtering by date.
 #$beforeDate = (Get-Date -Year 2020 -Month 6 -Day 1)
@@ -124,7 +129,7 @@ try {
   else {
     if ($user) {
       if ($password) {
-        $password = ConvertTo-SecureString $password -AsPlainText -Force
+        [SecureString]$password = ConvertTo-SecureString -String  $password -AsPlainText -Force
         $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
       }
       else { $credential = Get-Credential -Username $user }
@@ -134,8 +139,12 @@ try {
     Connect-Rubrik -Server $server -Credential $credential
   }
 } catch {
-  Write-Error "Error connecting to cluster or with authentication."
-  Exit 1
+  try {
+    Write-Error "Error connecting to cluster or with authentication."
+    $html = "Error connecting to cluster or with authentication."
+    if ($sendEmail) { Send-MailMessage -To $emailTo -From $emailFrom -Subject $emailSubject -BodyAsHtml -Body $html -SmtpServer $SMTPServer -Port $SMTPPort }
+    Exit 1
+  } catch { Exit 1 }
 }
 ###### RUBRIK AUTHENTICATION - END ######
 
@@ -143,7 +152,12 @@ try {
 if ($csvDelete -eq '')
 {
   # Get all objects under Snapshot Management (On Demands, Relics, Unmanaged Objects)
-  $allSnapshotManagement = Get-RubrikUnmanagedObject
+  if ($graphQL) {
+    $query = '{"query":"query UnmanagedObjectV1{unmanagedObjectV1Connection{nodes{id name objectType physicalLocation{name}unmanagedStatus isRemote snapshotCount localStorage archiveStorage retentionSlaDomainName}}}"}'
+    $allSnapshotManagement = (Invoke-RubrikGraphQLCall -Body $query).unmanagedObjectV1Connection.nodes
+  } else {
+    $allSnapshotManagement = Get-RubrikUnmanagedObject
+  }
 
   # $snapshostList contains list of snapshots based on the filters
   $snapshotList = @()
@@ -154,7 +168,6 @@ if ($csvDelete -eq '')
     if (($objectType -eq '' -or $i.objectType -like $objectType) -and
         ($objectStatus -eq '' -or $i.unmanagedStatus -like $objectStatus))
     {
-
       # Get all snapshots for the object
       $objectSnapshots = Invoke-RubrikRESTCall -Method GET -Api $apiVer -Endpoint "unmanaged_object/$($i.id)/snapshot"
 
@@ -179,6 +192,8 @@ if ($csvDelete -eq '')
             objectType = $i.objectType
             objectUnmanagedStatus =$i.unmanagedStatus
             objectRetentionSlaDomainName = $i.retentionSlaDomainName
+            objectTotalLocalStorageGB = [math]::round($i.localStorage / 1000000000, 3)
+            objectTotalArchiveStorageGB = [math]::round($i.archiveStorage / 1000000000, 3)
             objectTotalLocalStorage = $i.localStorage
             objectTotalArchiveStorage = $i.archiveStorage
             objectID = $i.id
