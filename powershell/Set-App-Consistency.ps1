eb8e44b36c1a36ad787b4fa1e6a4df0a88e3446f
Steven Tong <steven.c.tong@gmail.com>
2022-06-30 07:46:24 -0700

    Add Set-App-Consistency.ps1

--- powershell/Set-App-Consistency.ps1
+++ powershell/Set-App-Consistency.ps1
@@ -1,4 +1,28 @@
-# This script will bulk set the consistency level of a VM
+#requires -modules Rubrik
+
+# https://build.rubrik.com
+# https://www.rubrik.com/blog/get-started-rubrik-powershell-module/
+# https://github.com/rubrikinc/rubrik-scripts-for-powershell
+
+<#
+.SYNOPSIS
+This script will bulk set the consistency level of a VM
+
+.DESCRIPTION
+This script will bulk set the consistency level of a VM
+
+.NOTES
+Written originally by Chris Lunmah for community usage
+Updated by Steven Tong
+GitHub: stevenctong
+Date: 6/30/22
+
+First connect to a Rubrik cluster using "Connect-Rubrik".
+Update the parameters below as needed for SLA or consistency setting.
+
+.EXAMPLE
+./Set-App-Consistency.ps1
+#>
 
 # Get a list of Nutanix VMs
 # For VMware, use "Get-RubrikVM"
@@ -12,6 +36,7 @@ $consistency = 'ApplicationConsistent'
 
 foreach($vm in $vms)
 {
+  # If you want to set consistency level regardless of SLA, comment out this if statement block
   if($vm.effectiveSlaDomainName -eq $sla)
   {
     Write-Host "$($vm.name)" -foregroundcolor green
