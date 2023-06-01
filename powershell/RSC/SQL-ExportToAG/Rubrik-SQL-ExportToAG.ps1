0c04da1e95b91dfdff3e88257a357450306fd30e
Steven Tong <steven.c.tong@gmail.com>
2023-06-01 05:46:34 -0700

    Added Rubrik-SQL-ExportToAG

--- powershell/RSC/SQL-ExportToAG/Rubrik-SQL-ExportToAG.ps1
+++ powershell/RSC/SQL-ExportToAG/Rubrik-SQL-ExportToAG.ps1
@@ -70,151 +70,151 @@ if ($AddToAG -eq $true) {
   }
   exit
 }
-#
-# Connect-RubrikSecurityCloud -ServiceAccountPath $ServiceAccountPath
-#
-# Write-Host "Refreshing the SQL DB Hosts... (45 secs)" -foregroundcolor green
-#
-# $QueryParms = @{
-#     "hostname" = "$($PrimarySQLServerInstance)"
-#     "instance" = "MSSQLSERVER"
-# }
-# $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
-#
-# $QueryParms = @{
-#     "id" = "$($RSCMSQLInstance.id)"
-# }
-#  Invoke-RubrikQuery -Path ./PhysicalHostRefreshMutation.gql -QueryParams $QueryParms
-#
-# foreach ($SQLInstance in $SecondarySQLServerInstance){
-#     $QueryParms = @{
-#         "hostname" = "$($SQLInstance)"
-#         "instance" = "MSSQLSERVER"
-#     }
-#     $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
-#
-#     $QueryParms = @{
-#         "id" = "$($RSCMSQLInstance.id)"
-#     }
-#     Invoke-RubrikQuery -Path ./PhysicalHostRefreshMutation.gql -QueryParams $QueryParms
-# }
-# Start-Sleep -Seconds 45
-#
-# Write-Host "Getting ID of AG: $AvailabilityGroupName" -foregroundcolor green
-#
-# $QueryParms = @{
-#     "AvailabilityGroupName" = $($AvailabilityGroupName)
-# }
-# $RSCMSSQLAvailabilityGroup = Invoke-RubrikQuery -Path ./GetMSSQLAvailabilityGroup.gql -QueryParams $QueryParms
-#
-# if ($RSCMSSQLAvailabilityGroup.id -eq $null) {
-#     Write-Error "No AG found with name: $AvailabilityGroupName"
-# }
-#
-# if ($RSCMSSQLAvailabilityGroup.count -gt 1) {
-#     Write-Error "Multiple AG found with name: $AvailabilityGroupName"
-# }
-# $RSCMSSQLAvailabilityGroup
-#
-# Write-Host "Getting ID of DB: $DatabaseName on the AG."
-#
-# $QueryParms = @{
-#     "fid" = "$($RSCMSSQLAvailabilityGroup.id)"
-#     "DatabaseName" = "$($DatabaseName)"
-# }
-# $RSCMSSQLDatabase = Invoke-RubrikQuery -Path ./GetMSSQLDatabaseFromAvailabilityGroup.gql -QueryParams $QueryParms
-#
-# if ($RSCMSSQLDatabase.logicalChildConnection.nodes.count -eq 0) {
-#     Write-Error "No database found with name: $DatabaseName, on AG: $AvailabilityGroupName"
-# }
-# $RSCMSSQLDatabase.logicalChildConnection.nodes
-#
-# Write-Host "Getting the latest recovery point." -foregroundcolor green
-#
-# $QueryParms = @{
-#     "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
-# }
-# $RSCMSSQLRecoverableRange = Invoke-RubrikQuery -Path ./MssqlDatabaseDetailsRecoverableRangesQuery.gql -QueryParams $QueryParms | Sort-Object {$_.data.endTime}
-# $RSCMSSQLDatabaseLatestRecoveryPoint = ($RSCMSSQLRecoverableRange.data.endTime[-1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
-# $RSCMSSQLDatabaseLatestRecoveryPoint
-#
-# Write-Host "Getting the original DB file locations." -foregroundcolor green
-#
-# $QueryParms = @{
-#     "input" = @{
-#         "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
-#         "time" = $($RSCMSSQLDatabaseLatestRecoveryPoint)
-#     }
-# }
-# $RSCMSSQLFileLocations = Invoke-RubrikQuery -Path ./AllMssqlDatabaseRestoreFilesQuery.gql -QueryParams $QueryParms
-#
-# $TargetFilePaths = @()
-#
-# foreach ($DBfile in $RSCMSSQLFileLocations.items) {
-#     $originalName = [system.io.fileinfo]$DBFile.originalName
-#     $newFilename = $originalName.Basename + $FilenameAppend +  $originalName.Extension
-#     $targetFilePath = @{
-#         "newFilename" = $newFilename
-#         "exportPath" = $DBfile.originalPath
-#         "logicalName" = $DBFile.logicalName
-#     }
-#     $TargetFilePaths += $targetFilePath
-# }
-#
-# $targetFilePaths
-#
-# Write-Host "Initiating the restore to the secondary replicas and keeping in restoring mode." -foregroundcolor green
-#
-# foreach ($SQLInstance in $SecondarySQLServerInstance){
-#     $QueryParms = @{
-#         "hostname" = "$($SQLInstance)"
-#         "instance" = "MSSQLSERVER"
-#     }
-#     $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
-#     $RSCMSQLInstance
-#
-#     $QueryParms = @{
-#         "input" = @{
-#             "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
-#             "config" = @{
-#                 "recoveryPoint" = @{
-#                     "date" = "$($RSCMSSQLDatabaseLatestRecoveryPoint)"
-#                 }
-#                 "targetInstanceId" = "$($RSCMSQLInstance.physicalChildConnection.nodes.id)"
-#                 "targetDatabaseName" = "$($TargetDatabaseName)"
-#                 "targetDataFilePath" = ""
-#                 "targetLogFilePath" = ""
-#                 "targetFilePaths" = $TargetFilePaths
-#                 "allowOverwrite" = $true
-#                 "finishRecovery" = $false
-#             }
-#         }
-#     }
-#     Invoke-RubrikQuery -Path ./MssqlDatabaseExportMutation.gql -QueryParams $QueryParms
-# }
-#
-# Write-Host "Initiating the restore to the primary replica." -foregroundcolor green
-#
-# $QueryParms = @{
-#     "hostname" = "$($PrimarySQLServerInstance)"
-#     "instance" = "MSSQLSERVER"
-# }
-# $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
-# $RSCMSQLInstance
-#
-# $QueryParms = @{
-#     "input" = @{
-#         "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
-#         "config" = @{
-#             "recoveryPoint" = @{"date" = "$($RSCMSSQLDatabaseLatestRecoveryPoint)"}
-#             "targetInstanceId" = "$($RSCMSQLInstance.physicalChildConnection.nodes.id)"
-#             "targetDatabaseName" = "$($TargetDatabaseName)"
-#             "targetDataFilePath" = ""
-#             "targetLogFilePath" = ""
-#             "targetFilePaths" = $TargetFilePaths
-#             "allowOverwrite" = $true
-#             "finishRecovery" = $true
-#         }
-#     }
-# }
-# Invoke-RubrikQuery -Path ./MssqlDatabaseExportMutation.gql -QueryParams $QueryParms
+
+Connect-RubrikSecurityCloud -ServiceAccountPath $ServiceAccountPath
+
+Write-Host "Refreshing the SQL DB Hosts... (45 secs)" -foregroundcolor green
+
+$QueryParms = @{
+    "hostname" = "$($PrimarySQLServerInstance)"
+    "instance" = "MSSQLSERVER"
+}
+$RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
+
+$QueryParms = @{
+    "id" = "$($RSCMSQLInstance.id)"
+}
+ Invoke-RubrikQuery -Path ./PhysicalHostRefreshMutation.gql -QueryParams $QueryParms
+
+foreach ($SQLInstance in $SecondarySQLServerInstance){
+    $QueryParms = @{
+        "hostname" = "$($SQLInstance)"
+        "instance" = "MSSQLSERVER"
+    }
+    $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
+
+    $QueryParms = @{
+        "id" = "$($RSCMSQLInstance.id)"
+    }
+    Invoke-RubrikQuery -Path ./PhysicalHostRefreshMutation.gql -QueryParams $QueryParms
+}
+Start-Sleep -Seconds 45
+
+Write-Host "Getting ID of AG: $AvailabilityGroupName" -foregroundcolor green
+
+$QueryParms = @{
+    "AvailabilityGroupName" = $($AvailabilityGroupName)
+}
+$RSCMSSQLAvailabilityGroup = Invoke-RubrikQuery -Path ./GetMSSQLAvailabilityGroup.gql -QueryParams $QueryParms
+
+if ($RSCMSSQLAvailabilityGroup.id -eq $null) {
+    Write-Error "No AG found with name: $AvailabilityGroupName"
+}
+
+if ($RSCMSSQLAvailabilityGroup.count -gt 1) {
+    Write-Error "Multiple AG found with name: $AvailabilityGroupName"
+}
+$RSCMSSQLAvailabilityGroup
+
+Write-Host "Getting ID of DB: $DatabaseName on the AG."
+
+$QueryParms = @{
+    "fid" = "$($RSCMSSQLAvailabilityGroup.id)"
+    "DatabaseName" = "$($DatabaseName)"
+}
+$RSCMSSQLDatabase = Invoke-RubrikQuery -Path ./GetMSSQLDatabaseFromAvailabilityGroup.gql -QueryParams $QueryParms
+
+if ($RSCMSSQLDatabase.logicalChildConnection.nodes.count -eq 0) {
+    Write-Error "No database found with name: $DatabaseName, on AG: $AvailabilityGroupName"
+}
+$RSCMSSQLDatabase.logicalChildConnection.nodes
+
+Write-Host "Getting the latest recovery point." -foregroundcolor green
+
+$QueryParms = @{
+    "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
+}
+$RSCMSSQLRecoverableRange = Invoke-RubrikQuery -Path ./MssqlDatabaseDetailsRecoverableRangesQuery.gql -QueryParams $QueryParms | Sort-Object {$_.data.endTime}
+$RSCMSSQLDatabaseLatestRecoveryPoint = ($RSCMSSQLRecoverableRange.data.endTime[-1]).ToUniversalTime().ToString('yyyy-MM-ddTHH:mm:ss.fffZ')
+$RSCMSSQLDatabaseLatestRecoveryPoint
+
+Write-Host "Getting the original DB file locations." -foregroundcolor green
+
+$QueryParms = @{
+    "input" = @{
+        "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
+        "time" = $($RSCMSSQLDatabaseLatestRecoveryPoint)
+    }
+}
+$RSCMSSQLFileLocations = Invoke-RubrikQuery -Path ./AllMssqlDatabaseRestoreFilesQuery.gql -QueryParams $QueryParms
+
+$TargetFilePaths = @()
+
+foreach ($DBfile in $RSCMSSQLFileLocations.items) {
+    $originalName = [system.io.fileinfo]$DBFile.originalName
+    $newFilename = $originalName.Basename + $FilenameAppend +  $originalName.Extension
+    $targetFilePath = @{
+        "newFilename" = $newFilename
+        "exportPath" = $DBfile.originalPath
+        "logicalName" = $DBFile.logicalName
+    }
+    $TargetFilePaths += $targetFilePath
+}
+
+$targetFilePaths
+
+Write-Host "Initiating the restore to the secondary replicas and keeping in restoring mode." -foregroundcolor green
+
+foreach ($SQLInstance in $SecondarySQLServerInstance){
+    $QueryParms = @{
+        "hostname" = "$($SQLInstance)"
+        "instance" = "MSSQLSERVER"
+    }
+    $RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
+    $RSCMSQLInstance
+
+    $QueryParms = @{
+        "input" = @{
+            "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
+            "config" = @{
+                "recoveryPoint" = @{
+                    "date" = "$($RSCMSSQLDatabaseLatestRecoveryPoint)"
+                }
+                "targetInstanceId" = "$($RSCMSQLInstance.physicalChildConnection.nodes.id)"
+                "targetDatabaseName" = "$($TargetDatabaseName)"
+                "targetDataFilePath" = ""
+                "targetLogFilePath" = ""
+                "targetFilePaths" = $TargetFilePaths
+                "allowOverwrite" = $true
+                "finishRecovery" = $false
+            }
+        }
+    }
+    Invoke-RubrikQuery -Path ./MssqlDatabaseExportMutation.gql -QueryParams $QueryParms
+}
+
+Write-Host "Initiating the restore to the primary replica." -foregroundcolor green
+
+$QueryParms = @{
+    "hostname" = "$($PrimarySQLServerInstance)"
+    "instance" = "MSSQLSERVER"
+}
+$RSCMSQLInstance = Invoke-RubrikQuery -Path ./GetMSSQLInstance.gql -QueryParams $QueryParms
+$RSCMSQLInstance
+
+$QueryParms = @{
+    "input" = @{
+        "id" = "$($RSCMSSQLDatabase.logicalChildConnection.nodes[0].id)"
+        "config" = @{
+            "recoveryPoint" = @{"date" = "$($RSCMSSQLDatabaseLatestRecoveryPoint)"}
+            "targetInstanceId" = "$($RSCMSQLInstance.physicalChildConnection.nodes.id)"
+            "targetDatabaseName" = "$($TargetDatabaseName)"
+            "targetDataFilePath" = ""
+            "targetLogFilePath" = ""
+            "targetFilePaths" = $TargetFilePaths
+            "allowOverwrite" = $true
+            "finishRecovery" = $true
+        }
+    }
+}
+Invoke-RubrikQuery -Path ./MssqlDatabaseExportMutation.gql -QueryParams $QueryParms
